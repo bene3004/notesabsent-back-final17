d2d67fb7279b097766b4422802e0439d6e39b625
bene3004 <kandler.benedikt@gmail.com>
2025-05-07 19:01:17 +0500

    7

--- note-service/main.go
+++ note-service/main.go
@@ -14,33 +14,23 @@ func main() {
 	router := gin.Default()
 
 	protected := router.Group("/notes")
-	protected.Use(middleware.Auth())
+	protected.Use(middleware.Auth)
 	{
 		protected.GET("/", controllers.GetAllNotes)
 		protected.POST("/", controllers.AddNote)
 		protected.GET("/:id", controllers.GetNoteByID)
 		protected.PUT("/:id", controllers.UpdateNote)
 		protected.DELETE(":id", controllers.DeleteNote)
-	}
-
-	protected1 := router.Group("/comments")
-	protected1.Use(middleware.Auth())
-	{
-		protected1.GET("/", controllers.GetAllComments)
-		protected1.POST("/", controllers.AddComment)
-		protected1.GET("/:id", controllers.GetCommentByID)
-		protected1.PUT("/:id", controllers.UpdateComment)
-		protected1.DELETE(":id", controllers.DeleteComment)
-	}
-
-	protected2 := router.Group("/status")
-	protected2.Use(middleware.Auth())
-	{
-		protected2.GET("/", controllers.GetAllStatus)
-		protected2.POST("/", controllers.AddStatus)
-		protected2.GET("/:id", controllers.GetStatusByID)
-		protected2.PUT("/:id", controllers.UpdateStatus)
-		protected2.DELETE(":id", controllers.DeleteStatus)
+		protected.GET("/", controllers.GetAllComments)
+		protected.POST("/", controllers.AddComment)
+		protected.GET("/:id", controllers.GetCommentByID)
+		protected.PUT("/:id", controllers.UpdateComment)
+		protected.DELETE(":id", controllers.DeleteComment)
+		protected.GET("/", controllers.GetAllStatus)
+		protected.POST("/", controllers.AddStatus)
+		protected.GET("/:id", controllers.GetStatusByID)
+		protected.PUT("/:id", controllers.UpdateStatus)
+		protected.DELETE(":id", controllers.DeleteStatus)
 	}
 
 	router.Run(":8081")
