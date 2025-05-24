6b4d4e82e3dd7a5615794e38533573f18032b387
c4vvy <kandler.benedikt@gmail.com>
2025-05-24 08:19:43 +0500

    add17

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 4001,
-    proxy: {
-      '/auth': {target: 'http://localhost:8080', changeOrigin: true},
-      '/notes': {target: 'http://localhost:8081', changeOrigin: true},
-    },
-  },
 });
